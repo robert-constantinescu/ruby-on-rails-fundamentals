5d3d6d61cb76ee29fe88b128c692c20dff8a4751
Robert <constantinescu.robert94@gmail.com>
2021-08-12 21:26:56 +0300

    making sure that email is saved with downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 
-
+  # the self.email is referencing each object of the User class,
+  # so when it comes to the email field of any User, it will replace it with a downcase version
+  before_save { self.email = email.downcase }
   has_many :reports
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
