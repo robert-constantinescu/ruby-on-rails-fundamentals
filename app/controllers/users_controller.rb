8da0f434c69f6f7e9e6c138a7b4d90ad93a53646
Robert <constantinescu.robert94@gmail.com>
2021-08-13 18:48:56 +0300

    added a profile page for users and displayed the user reports and avatar under it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -13,6 +17,21 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(whitelist_and_extract_user_params)
+      flash[:notice] = 'Account was successfully updated'
+      redirect_to reports_path
+    else
+      render 'edit'
+    end
+
   end
 
 
