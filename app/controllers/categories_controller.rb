d3fb4c1c4ee9e9d995c6fe6a24d27fcd04f7ff55
Robert <constantinescu.robert94@gmail.com>
2021-08-14 21:46:23 +0300

    added logic: only admin users can create new categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
 
 
+  before_action :require_admin, except: [:index, :show]
   before_action :set_category, only: [:new]
 
   def new; end
@@ -32,4 +33,12 @@ class CategoriesController < ApplicationController
   def extract_params
     params.require(:category).permit(:name)
   end
+
+  def require_admin
+    unless logged_in? && current_user.admin?
+      flash[:alert] = 'Only admin can create new categories'
+      redirect_to categories_path
+    end
+  end
+
 end
